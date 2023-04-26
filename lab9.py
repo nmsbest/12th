f9cd5dd7aa60261563f2082363a3f86adfaa2980
nmsbest <nmsbest@proton.me>
2023-04-26 09:40:36 +0300

    changes

--- lab9.py
+++ lab9.py
@@ -1,3 +1,3 @@
-# create a binary file with name and rol number. search for given roll number and display name , if not found display appropriate message
+# create a binary file with name and roll number. search for given roll number and display name , if not found display appropriate message
 # Credits: Dhruv , XII-D
 
