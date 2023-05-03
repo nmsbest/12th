cd662c337e3e7438877604bc89e7de5d063a44a5
nmsbest <nmsbest@proton.me>
2023-05-03 09:12:46 +0300

    changes

--- lab3.py
+++ lab3.py
@@ -1,14 +1,14 @@
 # Py Program to find and display the prime number bw 2 to N . Pass M as arg to method
 # Credits: Dhruv , XII-D
 
-def get_prime_number(num1):
-    for num in range(2, num1 + 1):
-        if num > 1:
-            for i in range(2, num+1):
-                if (num % i) == 0:
-                    break
-            else:
-                print(num)
+def find_primes_between(m, n):
+    primes = []
+    for num in range(2, n+1):
+        if all(num%i!=0 for i in range(2, int(num**0.5)+1)):
+            primes.append(num)
+    return [p for p in primes if p >= m]
 
-number1 = int(input("Enter The Num: "))
-get_prime_number(number1)
+N = int(input("Enter the value of N: "))
+M = int(input("Enter the value of M: "))
+prime_numbers = find_primes_between(M, N)
+print("Prime numbers between", M, "and", N, "are:", prime_numbers)
