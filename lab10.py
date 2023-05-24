3f846e6c6ae93408b6667babf74aa4924fe8a0bf
nmsbest <nmsbest@proton.me@>
2023-05-24 09:44:31 +0300

    new

--- lab10.py
+++ lab10.py
@@ -1,2 +1,32 @@
 # read a text file and display the number of vowels/consonants/uppercase/lowercase chars in file
 # Credits: Dhruv , XII-D
+
+def count_chars(file_path):
+    vowels = "aeiouAEIOU"
+    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+    upper_count = 0
+    lower_count = 0
+    vowel_count = 0
+    consonant_count = 0
+
+    with open(file_path, "r") as file:
+        data = file.read()
+        for char in data:
+            if char.isupper():
+                upper_count += 1
+            elif char.islower():
+                lower_count += 1
+            if char in vowels:
+                vowel_count += 1
+            elif char in consonants:
+                consonant_count += 1
+
+    print("Number of vowels:", vowel_count)
+    print("Number of consonants:", consonant_count)
+    print("Number of uppercase characters:", upper_count)
+    print("Number of lowercase characters:", lower_count)
+
+
+# Example usage
+file_path = "example.txt"
+count_chars(file_path)
--- lab11.py
+++ lab11.py
@@ -1,2 +1,13 @@
 # py program to search for and replace a substring
 # Credits: Dhruv , XII-D
+
+def replace_substring(string, substring, replacement):
+    new_string = string.replace(substring, replacement)
+    return new_string
+
+# Example usage
+original_string = "The quick brown fox jumps over the lazy dog"
+new_string = replace_substring(original_string, "quick", "slow")
+
+print("Original string:", original_string)
+print("New string:", new_string)
