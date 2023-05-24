1daef392118398ab45a9616741a0e8db95e0ae9f
nmsbest <nmsbest@proton.me@>
2023-05-24 09:49:51 +0300

    new

---
+++ lab14.py
@@ -0,0 +1,11 @@
+"""
+write a py func MYSTACK(d) , which should accept a dict d of the form roll: name argument (existing dict) . The func should create a stack MYNAME having all the names containing `s` or `S`. Also, write a function POPSTACK() 
+
+which should pop and print all the elements of stack MYNAME, and print stack empty at the end.
+
+for eg, if the dict d is {1:"sunil",2:"naman",4:"anish"}
+
+then MYNAME should be ['sunil','anish']
+
+Output of POPSTACK() func should be Anish Sunil Stack Empty
+"""
