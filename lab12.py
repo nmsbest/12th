aaa303970a24ab1dbd34a74d477358ae93980224
nmsbest <nmsbest@proton.me@>
2023-05-24 09:38:48 +0300

    new

--- lab12.py
+++ lab12.py
@@ -1,2 +1,21 @@
 # Write a func which transposes a list of numbers which are divisible by 5 to new stack.
 # Credits: Dhruv , XII-D
+
+original_list = []
+refined_list = []
+
+def add():
+    n = int(input("How many numbers do you want to enter: "))
+    for i in range(n):
+        num = int(input("Number: "))
+        original_list.append(num)
+    print(original_list,refined_list)
+
+def refine():
+    for i in original_list:
+        if i%5 == 0:
+            refined_list.append(i)
+    print(original_list,refined_list)
+    
+add()
+refine()
